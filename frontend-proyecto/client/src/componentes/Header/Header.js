0d63c6afcd80110c4fee78db244793e47891bb67
SantiagoP <santiagoplanasm@gmail.com>
2023-05-30 16:00:02 -0300

    barra de busqueda front

---
+++ frontend-proyecto/client/src/componentes/Header/Header.js
@@ -0,0 +1,75 @@
+
+import "./Header.css"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { DateRange } from "react-date-range";
+import { format } from "date-fns";
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
+
+
+const Header = ({ type }) => {
+    const [destination, setDestination] = useState("");
+    const [openDate, setOpenDate] = useState(false);
+    const [date, setDate] = useState([
+      {
+        startDate: new Date(),
+        endDate: new Date(),
+        key: "selection",
+      },
+    ]);
+    
+    
+  
+    const navigate = useNavigate();
+  
+    
+  
+    const handleSearch = () => {
+      navigate("/lista", { state: { destination, date} });
+    };
+
+  return (
+    <div className="header">
+        <div
+        className={
+          type === "list" ? "headerContainer listMode" : "headerContainer"
+        }
+      >
+        <div className="headerSearch">
+             
+              <div className="headerSearchItem">
+                
+                <span
+                  onClick={() => setOpenDate(!openDate)}
+                  className="headerSearchText"
+                >{`${format(date[0].startDate, "yyyy/MM/dd")} to ${format(
+                  date[0].endDate,
+                  "yyyy/MM/dd"
+                )}`}</span>
+                {openDate && (
+                  <DateRange
+                    editableDateInputs={true}
+                    onChange={(item) => setDate([item.selection])}
+                    moveRangeOnFirstSelection={false}
+                    ranges={date}
+                    className="date"
+                    minDate={new Date()}
+                  />
+                )}
+              </div>
+              <div className="headerSearchItem">
+                <button className="headerBtn" onClick={handleSearch}>
+                  Search
+                </button>
+              </div>
+            </div>
+      
+    </div>
+    </div>
+  )
+
+}
+
+export default Header
--- frontend-proyecto/client/src/pages/Lista.js
+++ frontend-proyecto/client/src/pages/Lista.js
@@ -1,8 +1,6 @@
 import React from "react";
 
 import '../App.css';
-
-import {useParams} from "react-router-dom";
   
 const Lista = () => {
   return (
