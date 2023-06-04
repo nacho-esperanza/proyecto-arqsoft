43d8e283803c2104919571849dba9959e69a02c8
frun903 <frun903@gmail.com>
2023-06-04 20:10:00 -0300

    Modificaciones Login SignUp

--- frontend-proyecto/client/src/pages/SignUp.js
+++ frontend-proyecto/client/src/pages/SignUp.js
@@ -5,10 +5,43 @@ import './SignUp.css';
 const SignUp = () => {
  
   return (
-    <div className="signup-container">
-      <h2>Sign Up</h2>
+
+    
+
+
+<div className="signup-container-1">
+
+<h1>Caminos del Viento</h1>
+
+<div className="signup-container">
+
+<h2>
+        Registro de Usuario
+</h2>
      
+
+<form>
+
+<input type="text" placeholder="Nombre"/>
+
+<input type="texto" placeholder="Apellido"/>
+
+<input type="email" placeholder="Email"/>
+
+<input type="password" placeholder="Contraseña"/>
+
+<input type="password" placeholder="Repita Contraseña"/>
+
+<button type="submit">Registrate</button>
+
+</form>
+
+<p>Ya tengo Usuario</p>
+
+<button type="submit" onClick={() => window.location.href = '/login'}>Iniciar Sesion</button>
+
     </div>
+</div>
   );
 };
 
