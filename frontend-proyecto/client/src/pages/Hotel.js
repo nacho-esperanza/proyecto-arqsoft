d01d540bdef1edb4278e6c1105b1071fcebbea32
frun903 <frun903@gmail.com>
2023-06-04 21:13:57 -0300

    Modificaciones en Hotel.js

--- frontend-proyecto/client/src/pages/Hotel.js
+++ frontend-proyecto/client/src/pages/Hotel.js
@@ -1,12 +1,16 @@
 import React from "react";
 
-import '../App.css';
-  
+import './Home.css';  
 const Hotel = () => {
   return (
-    <div>
-    hotel
-    </div>
+    <div className="home-container">
+    <div className="menu_titulo">Menú Título</div>
+    <div className="nombre_hotel">Nombre del Hotel</div>
+    <div className="hotel_descripcion">Descripción del Hotel</div>
+    <div className="hotel_img">Imagen del Hotel</div>
+    <div className="hotel_caracteristica">Características del Hotel</div>
+    <div className="hotel_Precio">Precio del Hotel</div>
+  </div>
   );
 };
   
