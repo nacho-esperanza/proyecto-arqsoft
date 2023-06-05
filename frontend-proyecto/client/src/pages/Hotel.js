14020092a9fe006139a9d85ff5eaa33a189d0a89
frun903 <frun903@gmail.com>
2023-06-04 21:45:35 -0300

    Modificaciones en Hotel.js hotel,css insercion de grilla

--- frontend-proyecto/client/src/pages/Hotel.js
+++ frontend-proyecto/client/src/pages/Hotel.js
@@ -4,12 +4,39 @@ import './Hotel.css';
 const Hotel = () => {
   return (
     <div className="home-container">
-    <div className="menu_titulo">Menú Título</div>
+    
+    
+    <div className="menu_titulo">
+    <p>Username</p>
+    <h1>Caminos del Viento</h1>
+
+    </div>
+
+
+
     <div className="nombre_hotel">Nombre del Hotel</div>
-    <div className="hotel_descripcion">Descripción del Hotel</div>
-    <div className="hotel_img">Imagen del Hotel</div>
-    <div className="hotel_caracteristica">Características del Hotel</div>
-    <div className="hotel_Precio">Precio del Hotel</div>
+    <div className="hotel_descripcion">Descripción del Hotel
+    
+    <p>Aquí va la descripción del hotel. Puedes agregar información sobre las características, servicios y ubicación del hotel.Proporciona detalles atractivos y convincentes para que los usuarios se sientan interesados en hospedarse en tu hotel.Aquí va la descripción del hotel. Puedes agregar información sobre las características, servicios y ubicación del hotel.Proporciona detalles atractivos y convincentes para que los usuarios se sientan interesados en hospedarse en tu hotel.Aquí va la descripción del hotel. Puedes agregar información sobre las características, servicios y ubicación del hotel.Proporciona detalles atractivos y convincentes para que los usuarios se sientan interesados en hospedarse en tu hotel.Aquí va la descripción del hotel. Puedes agregar información sobre las características, servicios y ubicación del hotel.Proporciona detalles atractivos y convincentes para que los usuarios se sientan interesados en hospedarse en tu hotel.   </p>
+    
+    </div>
+    
+    <div className="hotel_img">Imagen del Hotel
+    
+    </div>
+    
+    
+    <div className="hotel_caracteristica">Características del Hotel:
+    <li>Caracteristica</li>
+    <li>Caracteristica</li>
+    <li>Caracteristica</li>
+    <li>Caracteristica</li>
+    </div>
+    <div className="hotel_Precio">Precio del Hotel
+    <br></br>
+    <br></br>
+    <button>Reservar</button>
+    </div>
   </div>
   );
 };
