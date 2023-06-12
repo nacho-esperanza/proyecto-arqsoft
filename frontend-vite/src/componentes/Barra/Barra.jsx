730f044a087176a37797a2e58631d9e0e17bf9bc
nacho-esperanza <2112828@ucc.edu.ar>
2023-06-12 04:23:17 -0300

    Barra SUPER MEJORADA

--- frontend-vite/src/componentes/Barra/Barra.jsx
+++ frontend-vite/src/componentes/Barra/Barra.jsx
@@ -1,17 +1,72 @@
-import "./Barra.css"
+import React, { useEffect, useState } from 'react';
+
+import "./Barra.css";
 
 const Barra = () => {
+  const userId = localStorage.getItem("user_id");
+  const token = localStorage.getItem("token");
+  const [user, setUser] = useState(null);
+  const isAdmin = localStorage.getItem("isAdmin");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`http://localhost:8090/user/${userId}`);
+      if (response.ok) {
+        const data = await response.json();
+        setUser(data);
+      }
+    };
+
+    fetchData();
+  }, [userId]);
+  const handleLogin = () => {
+    window.location.href = '/login';
+  };
+
+  const handleSignup = () => {
+    window.location.href = '/signup';
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user_id");
+    window.location.href = '/';
+  };
+
   return (
     <div className="NaveBa">
       <div className="NaveC">
         <h1 className="logo">Caminos del Viento</h1>
         <div className="NaveIt">
-          <button className="NaveBo"  onClick={() => window.location.href = '/signup'}>Registrar</button>
-          <button className="NaveBo" onClick={() => window.location.href = '/login'}>Iniciar sesion</button>
+          {user && (
+            <>
+              <h3>Hola {user.name}!</h3>
+              {isAdmin === "true" && (
+                <>
+                  {/* Mostrar opciones específicas para el administrador */}
+                  <button className="NaveBo" onClick={() => window.location.href = '/admin'}>Panel de Administrador</button>
+                </>
+              )}
+              {isAdmin === "false" && (
+                <>
+                  {/* Mostrar opciones específicas para el cliente */}
+                  <button className="NaveBo" onClick={() => window.location.href = '/misreservas'}>Mis Reservas</button>
+                </>
+              )}
+              <button className="NaveBo" onClick={handleLogout}>Cerrar sesión</button>
+            </>
+          )}
+          {!user && (
+            <>
+              {/* Mostrar opciones para un usuario no autenticado */}
+              <button className="NaveBo" onClick={handleSignup}>Registrar</button>
+              <button className="NaveBo" onClick={handleLogin}>Iniciar sesión</button>
+            </>
+          )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Barra
+export default Barra;
