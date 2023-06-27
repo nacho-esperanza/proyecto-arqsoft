5336cdaa1fa25eb933c3b16fdeb0ab1bd2246392
nacho-esperanza <2112828@ucc.edu.ar>
2023-06-26 23:28:34 -0300

    DOCKER SIN ANDAR AUN EL COMPOSE

---
+++ frontend-vite/src/pages/CrearHotel.jsx
@@ -0,0 +1,10 @@
+import React, {useEffect, useState} from 'react';
+import {useNavigate, useParams} from 'react-router-dom';
+
+import './CrearHotel.css';
+
+// Import react hot toast
+import toast, {Toaster} from 'react-hot-toast';
+
+const CrearHotel = () => {
+    
